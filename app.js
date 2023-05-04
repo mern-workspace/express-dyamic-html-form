ffc847db7510a7df92b50d48786989d3c628c772
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-05-04 08:50:54 +0530

    Add. Added static HTML page.

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const userPage = require('./user')
 
+// for static web page
+// app.use(express.static('public'))
+
+// for dynamic web page
 app.set("view engine","ejs")
 
 app.use(express.urlencoded({extended : true}))
 
 app.get('/',(request,response)=>{
     response.send(200)
+
+    // render static web page
+    // response.render('index')
 })
 
 app.use('/user',userPage)
