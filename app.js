75417c6a80d1a70323538561d2f58b3c92594fad
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-05-04 08:43:40 +0530

    Add. Added dynamic html form to add services and stored in array.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const userPage = require('./user')
+
+app.set("view engine","ejs")
+
+app.use(express.urlencoded({extended : true}))
 
 app.get('/',(request,response)=>{
     response.send(200)
 })
+
+app.use('/user',userPage)
+
 
 app.listen(3800)
---
+++ user.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/',(request,response)=>{
+    response.send("This is the user page")
+})
+
+router.get('/service',(request,response)=>{
+    response.render('users/service')
+})
+
+router.post('/',(request,response)=>{
+    isAValidValue = true
+    if(isAValidValue)
+    {
+        services.push({name:request.body.serviceName})
+        response.redirect(`/user/${services.length - 1}`)
+    }
+    else
+    {
+        console.log("error");
+        response.render('users/service', {serviceName : request.body.serviceName})
+        // response.render('users/service')
+    }
+    
+})
+
+router.get('/:id',(request,response)=>{
+    response.send(`Service Name Added with Id : ${request.params.id}`)
+})
+
+const services = [
+    {
+        name : 'Web Development'
+    }
+]
+
+router.param('id',(request,response,next,id)=>{
+    request.user = services[id]
+    next()
+})
+
+module.exports = router
