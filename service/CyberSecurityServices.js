671b09d1a6c6d66f58026c799efd9e4de6d024d0
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-05-04 20:50:18 +0530

    Update. Added service pages as router.

---
+++ service/CyberSecurityServices.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(request,response)=>{
+    response.send("This is Cyber Security Services")
+})
+
+module.exports = router
--- user.js
+++ user.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
+const WebDevelopmentServicePage = require('./service/WebDevelopmnetService')
+const CyberSecurityServicePage = require('./service/CyberSecurityServices')
+const MobileAppDevelopmentServicePage = require('./service/MobileAppDevelopmentService')
+const DigitalMarketingServicePage = require('./service/DigitalMarketingServices')
+const CloudBackupServicePage = require('./service/CloudBackupService')
+const FirewallServicePage = require('./service/FirewallServices')
+const NetworkingServicePage = require('./service/NetworkingService')
+const SaaSServicePage = require('./service/SaaS')
+
 router.get('/',(request,response)=>{
     response.send("This is the user page")
 })
@@ -25,7 +34,7 @@ router.post('/',(request,response)=>{
     
 })
 
-router.get('/:id',(request,response)=>{
+router.get('/:id([0-9]{1,2})',(request,response)=>{
     response.send(`Service Name Added with Id : ${request.params.id}`)
 })
 
@@ -60,5 +69,15 @@ router.param('id',(request,response,next,id)=>{
     request.user = services[id]
     next()
 })
+
+
+router.use('/webDevelopment',WebDevelopmentServicePage)
+router.use('/cyberSecurity',CyberSecurityServicePage)
+router.use('/mobileAppDevelopment',MobileAppDevelopmentServicePage)
+router.use('/digitalMarketing',DigitalMarketingServicePage)
+router.use('/cloudBackup',CloudBackupServicePage)
+router.use('/firewall',FirewallServicePage)
+router.use('/networking',NetworkingServicePage)
+router.use('/SaaS',SaaSServicePage)
 
 module.exports = router
